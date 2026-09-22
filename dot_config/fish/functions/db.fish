03cd75b1b647c70143cca15251397598c1cf75af
hirrrooo <99264185+hirrrooo@users.noreply.github.com>
2026-09-22 21:58:45 +0000

    remove fish as zsh is used now

--- dot_config/fish/functions/db.fish
+++
@@ -1,3 +0,0 @@
-function db --wraps='distrobox enter dog-box' --description 'alias db=distrobox enter dog-box'
-    distrobox enter dog-box $argv
-end
--- dot_config/fish/functions/vert.fish
+++
@@ -1,3 +0,0 @@
-function vert --wraps='hyprctl dispatch movecurrentworkspacetomonitor DP-2' --wraps='hyprctl dispatch movecurrentworkspacetomonitor DP-2 && exit' --description 'alias vert=hyprctl dispatch movecurrentworkspacetomonitor DP-2 && exit'
-    hyprctl dispatch movecurrentworkspacetomonitor DP-2 && exit $argv
-end
